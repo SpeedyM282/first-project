33af4a6fc2e7cbda022e446d8eb6ebcb041c516b
SpeedyM282 <a.rizakulov@student.inha.uz>
2022-11-25 14:33:04 +0500

    add background color change Button

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -3,8 +3,12 @@ import './style.css';
 
 export default class Button extends React.Component {
   render() {
+    const style = {
+      "backgroundColor": this.props.txt == 'Attack' && '#F75353',
+    }
+
     return (
-      <button className='btn' onClick={this.props.handleClick}>
+      <button className='btn' style={style} onClick={() => this.props.handleClick(this.props.txt)}>
         {this.props.txt}
       </button>
     );
