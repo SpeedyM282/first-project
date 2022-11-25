8cb2af95f4d316a8132c194fb2032566a24e070e
SpeedyM282 <a.rizakulov@student.inha.uz>
2022-11-25 14:31:53 +0500

    add background repaint functionality Square

--- src/components/Square/Square.js
+++ src/components/Square/Square.js
@@ -3,8 +3,15 @@ import './style.css';
 
 export default class Square extends React.Component {
   render() {
+    const map = {
+      [this.props.isChecked?.includes(this.props.id)]: '#4D5B9E',
+      [this.props.isVessel?.includes(this.props.id)]: '#F75353',
+      [this.props.isAttacked?.includes(this.props.id)]: '#EED202',
+      default: 'white'
+    }
+    const style = { "backgroundColor": map['true'] || map['default'] }
     return (
-      <div className='square-wrapper'></div>
+      <button onClick={() => this.props.handleClick(this.props.id)} className='square-wrapper' style={style} ></button >
     );
   }
 }
