e27703611cb1138057598aa7e9eddefd633a25de
SpeedyM282 <a.rizakulov@student.inha.uz>
2022-11-25 14:34:48 +0500

    add Popout modal

--- src/App.js
+++ src/App.js
@@ -1,66 +1,93 @@
 import React from 'react';
-import Button from './components/Button/Button';
-// import Field from './components/Field/Field';
 import StartPage from './pages/StartPage/StartPage';
 import ArrangementPage from './pages/ArrangementPage/ArrangementPage';
 import GamePage from './pages/GamePage/GamePage';
+import Button from './components/Button/Button';
+import Popout from './components/Popout/Popout';
 import './App.css';
 
 export default class App extends React.Component {
-  state = {
-    pageSwitch: 1,
+  constructor(props) {
+    super(props);
+    this.state = {
+      isModalOpen: false,
+      pageSwitch: 1,
+    };
   }
 
-  togglePage = () => {
+  componentDidMount() {
+    const data = {
+      isChecked: [],
+      isVessel: [],
+      isAttacked: [],
+      password: '',
+    };
+
+    localStorage.setItem('user1', JSON.stringify(data));
+    localStorage.setItem('user2', JSON.stringify(data));
+  }
+
+  togglePage = (val) => {
+    if (val == 'Confirm' || val == 'OK') {
+      this.setState(prevState => ({
+        isModalOpen: !prevState.isModalOpen
+      }));
+    }
+
     this.setState(prevState => {
       if (prevState.pageSwitch === -3) {
         return { pageSwitch: 1 };
-      } else {
+      } else if (val != 'OK') {
         return { pageSwitch: prevState.pageSwitch - 1 };
       }
     });
-    console.log(this.state.pageSwitch)
   }
 
   render() {
+    const txt = {
+      '1': 'Start',
+      '0': 'Confirm',
+      '-1': 'Confirm',
+      '-2': 'Play',
+      '-3': 'Restart'
+    }
+    const p = this.state.pageSwitch;
+
     return (
       <div className='App'>
-        {
-          this.state.pageSwitch === 1 ?
-            <StartPage
-              heading='Naval Combat'
-              description='Strategy type guessing game for two players'
-            /> :
-            this.state.pageSwitch === 0 ?
-              <ArrangementPage player='1' /> :
-              this.state.pageSwitch === -1 ?
-                <ArrangementPage player='2' /> :
-                this.state.pageSwitch === -2 ?
-                  <StartPage
-                    heading='Are you ready?'
-                    description='If you are ready click "play" button'
-                  /> :
-                  <GamePage />
+        {p === 1 &&
+          (<StartPage
+            heading={'Naval Combat'}
+            description={'Strategy type guessing game for two players.'}
+          />)}
+
+        {p === 0 && (<ArrangementPage player='1' />)}
+
+        {p === -1 && (<ArrangementPage player='2' />)}
+
+        {p === -2 &&
+          (<StartPage
+            heading='Are you ready?'
+            description='If you hit vessel the square will turn into red color otherwise yellow color. When you are ready click "play" button.'
+          />)
         }
 
-        {
-          true &&
-          <Button
-            handleClick={this.togglePage}
-            txt={
-              this.state.pageSwitch === 1 ?
-                'Start' :
-                this.state.pageSwitch === 0 ?
-                  'Done' :
-                  this.state.pageSwitch === -1 ?
-                    'Done' :
-                    this.state.pageSwitch === -2 ?
-                      'Play' :
-                      'Restart'
-            }
+        {p === -3 && <GamePage />}
+
+        <Button
+          handleClick={this.togglePage}
+          txt={txt[`${p}`]}
+        />
+
+        {this.state.isModalOpen && (
+          <Popout
+            id="modal"
+            isOpen={this.state.isModalOpen}
+            onClose={this.togglePage}
+            player={p == -1 ? '1' : '2'}
           />
-        }
+        )}
       </div>
-    );
+    )
   }
 }
